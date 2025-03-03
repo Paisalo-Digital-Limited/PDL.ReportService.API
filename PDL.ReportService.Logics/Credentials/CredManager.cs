523aaf1cdcb3fc81a6fa062766746961e168d21d
Satish Maurya <dotnetdev1@paisalo.in>
2025-03-03 11:45:15 +0530

    change

--- PDL.ReportService.Logics/Credentials/CredManager.cs
+++ PDL.ReportService.Logics/Credentials/CredManager.cs
@@ -20,9 +20,9 @@ namespace PDL.ReportService.Logics.Credentials
             try
             {
                 if (!islive)
-                    conStr = $"Data Source=192.168.10.2;Initial Catalog={db};User ID=;Password=;Connection Timeout=120;Trusted_Connection=False;MultipleActiveResultSets=True;Encrypt=false";
+                    conStr = $"Data Source=192.168.10.2;Initial Catalog={db};User ID=sa;Password=Sasqlserver2022@10.2;Connection Timeout=120;Trusted_Connection=False;MultipleActiveResultSets=True;Encrypt=false";
                 else
-                    conStr = $"Data Source=192.168.10.2;Initial Catalog={db};User ID=;Password=;Connection Timeout=120;Trusted_Connection=False;MultipleActiveResultSets=True;Encrypt=false";
+                    conStr = $"Data Source=192.168.10.2;Initial Catalog={db};User ID=sa;Password=Sasqlserver2022@10.2;Connection Timeout=120;Trusted_Connection=False;MultipleActiveResultSets=True;Encrypt=false";
 
 
                 newConn = new SqlConnection(conStr);
