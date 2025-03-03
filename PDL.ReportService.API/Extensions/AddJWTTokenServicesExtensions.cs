62019d15756d9d9f0fda7725035e10b30b7f4c15
Satish Maurya <dotnetdev1@paisalo.in>
2025-03-03 19:29:00 +0530

    change

---
+++ PDL.ReportService.API/Extensions/AddJWTTokenServicesExtensions.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
+namespace PDL.ReportService.API.Extensions
+{
+    public static class AddJWTTokenServicesExtensions
+    {
+        public static void AddJWTTokenServices(this IServiceCollection Services, IConfiguration Configuration)
+        {
+            // Add Jwt Setings
+            var bindJwtSettings = new JwtSettings();
+            Configuration.Bind("JsonWebTokenKeys", bindJwtSettings);
+            Services.AddSingleton(bindJwtSettings);
+
+
+            Services.AddAuthentication(options => {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options => {
+                options.SaveToken = true;
+                options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters()
+                {
+                    ValidateIssuerSigningKey = bindJwtSettings.ValidateIssuerSigningKey,
+                    IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(bindJwtSettings.IssuerSigningKey)),
+                    ValidateIssuer = bindJwtSettings.ValidateIssuer,
+                    ValidIssuer = bindJwtSettings.ValidIssuer,
+                    ValidateAudience = bindJwtSettings.ValidateAudience,
+                    ValidAudience = bindJwtSettings.ValidAudience,
+                    RequireExpirationTime = bindJwtSettings.RequireExpirationTime,
+                    ValidateLifetime = bindJwtSettings.RequireExpirationTime,
+                    ClockSkew = TimeSpan.FromDays(1),
+                };
+            });
+        }
+        public class JwtSettings
+        {
+            public bool ValidateIssuerSigningKey { get; set; }
+            public string IssuerSigningKey { get; set; }
+            public bool ValidateIssuer { get; set; } = true;
+            public string ValidIssuer { get; set; }
+            public bool ValidateAudience { get; set; } = true;
+            public string ValidAudience { get; set; }
+            public bool RequireExpirationTime { get; set; }
+            public bool ValidateLifetime { get; set; } = true;
+        }
+
+    }
+}
