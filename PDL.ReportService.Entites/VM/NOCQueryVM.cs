4ff23b128ac8f56b5f179ec48e3485d4dc86eaec
Mohit3001 <dotnetdev7@paisalo.in>
2025-03-07 14:09:09 +0530

    Post api create [NOC]

---
+++ PDL.ReportService.Entites/VM/NOCQueryVM.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PDL.ReportService.Entites.VM
+{
+    public class NOCQueryVM
+    {
+        public string Query { get; set; }
+        public IFormFile Image { get; set; }
+    }
+}
