7dc7d8fa664850376ba07678f918d31162c83fff
RajatJain9 <dotnetdev8@paisalo.in>
2025-03-20 11:15:02 +0530

    commit changes

--- PDL.ReportService.Entites/VM/BranchDashBoardVM.cs
+++ PDL.ReportService.Entites/VM/BranchDashBoardVM.cs
@@ -130,7 +130,6 @@ namespace PDL.ReportService.Entites.VM
         public string? Message { get; set; }
         public int? NotificationType { get; set; }
         public string? InternShipEndDate { get; set; }
-        public string? NoticePeriodEndDate { get; set; }
         public string? JoiningDate { get; set; }
     }
 
