2dda3257727588a92798c25facbf46df8faa59e6
Kartik <dotnetdev16@paisalo.in>
2025-06-18 18:41:03 +0530

    Feature: Implement API for RC Disbursement Transaction Report (#42)
    Co-authored-by: Kartik <user@KARTIKIT-DESKTO>

--- PDL.ReportService.Interfaces/Interfaces/IReports.cs
+++ PDL.ReportService.Interfaces/Interfaces/IReports.cs
@@ -19,6 +19,6 @@ namespace PDL.ReportService.Interfaces.Interfaces
         List<EMIInformationVM> GetEMIInformation(string smCode, string dbtype, string dbName, bool isLive, int PageNumber, int PageSize);
         List<LoanWithoutDisbVoucherVM> GetLoansWithoutDisbursements(string dDbName, string dbName, bool isLive, int PageNumber, int PageSize, out int TotalCount);
         List<DuplicateDIBVoucherVM> GetDuplicateDIBVouchers(string dbtype, string dbName, bool isLive, int PageNumber, int PageSize);
-
+        List<RcTransactionVM> GetRcDisbursementTransactionReport(string dDbName, string dbName, bool isLive, int PageNumber, int PageSize, out int TotalCount, DateTime fromDate, DateTime toDate, string creator);
     }
 }
