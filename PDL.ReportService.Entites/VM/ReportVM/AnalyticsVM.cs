03040beb4ae685104c0086b2009f9cd766cde5c6
Kartik <dotnetdev16@paisalo.in>
2025-07-03 14:38:03 +0530

    Fix: Add SubjectID, Verification and SolTrader in Consumer JSON (#57)
    Co-authored-by: Kartik <user@KARTIKIT-DESKTO>

--- PDL.ReportService.Entites/VM/ReportVM/AnalyticsVM.cs
+++ PDL.ReportService.Entites/VM/ReportVM/AnalyticsVM.cs
@@ -8,14 +8,6 @@ namespace PDL.ReportService.Entites.VM.ReportVM
 {
     public class AnalyticsVM
     {
-        public string SubjectId { get; set; }
-        public string Verification { get; set; }
-        public string SoleTrader { get; set; }
-        public string TransactionScore { get; set; }
-        public string ScoreArea { get; set; }
-        public string ScoreTranche { get; set; }
-        public string MonthlyAnalysisJson { get; set; }
-        public string PeriodAnalysisJson { get; set; }
-        public string InsightsJson { get; set; }
+        public ConsumerVM Consumer { get; set; }
     }
 }
