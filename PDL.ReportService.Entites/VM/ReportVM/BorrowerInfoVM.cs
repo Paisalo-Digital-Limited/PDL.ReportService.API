7dd8afa6af3e245393b5e58fbb884f04edcaa7dd
Kartik <dotnetdev16@paisalo.in>
2025-07-10 17:03:09 +0530

    Fix: Implement complete data of AccountAggregator (#61)
    Co-authored-by: Kartik <user@KARTIKIT-DESKTO>

--- PDL.ReportService.Entites/VM/ReportVM/BorrowerInfoVM.cs
+++ PDL.ReportService.Entites/VM/ReportVM/BorrowerInfoVM.cs
@@ -8,16 +8,6 @@ namespace PDL.ReportService.Entites.VM.ReportVM
 {
     public class BorrowerInfoVM
     {
-        public string FICode { get; set; }
-        public string SMCode { get; set; }
-        public string Name { get; set; }
-        public DateTime? DOB { get; set; }
-        public string Phone { get; set; }
-        public string Address { get; set; }
-        public string Email { get; set; }
-        public string PAN { get; set; }
-        public string BranchCode { get; set; }
-        public string IFSC { get; set; }
-        public string Status { get; set; }
+        
     }
 }
