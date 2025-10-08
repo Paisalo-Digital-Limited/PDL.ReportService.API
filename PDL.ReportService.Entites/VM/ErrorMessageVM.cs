0f509a72a5265f211be718102e426354b2368abe
ShivamSavita <120547087+ShivamSavita@users.noreply.github.com>
2025-10-08 18:08:32 +0530

    Implement JWT token version validation and invalidate tokens on password change (#76)
    * Changes has been done for cam genertion
    
    * changes done
    
    * Changes has been completed for CAM generation
    
    * Changes added for Auth Session Expiry
    
    * Implement JWT token version validation and invalidate tokens on password change

---
+++ PDL.ReportService.Entites/VM/ErrorMessageVM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PDL.Authentication.Entites.VM
+{
+    public class ErrorMessageVM
+    {
+        public string errormsg { get; set; }
+        public bool isValidate { get; set; }
+    }
+}
